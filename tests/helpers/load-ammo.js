cbea738b7d368d9511c248b33261feebb7440697
yandeu <20306025+yandeu@users.noreply.github.com>
2025-09-05 16:19:40 +0200

    Build type definitions

--- tests/helpers/load-ammo.js
+++ tests/helpers/load-ammo.js
@@ -1,5 +1,12 @@
+/// <reference types="../../builds/ammo.d.ts" />
+
 const { resolve } = require('path');
 
+/**
+ * 
+ * @param {*} options 
+ * @returns {Promise<Ammo>} ammo
+ */
 async function loadAmmo(options) {
   const { AMMO_PATH, PWD } = process.env;
   const path = AMMO_PATH ? resolve(PWD, AMMO_PATH) : '../../builds/ammo.js';
